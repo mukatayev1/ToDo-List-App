babfb5b1c714d1d518a28199e89539048671c40f
Aidos Mukatayev <mukatayev19@gmail.com>
2020-11-05 18:06:01 +0900

    Initial Commit

---
+++ ToDo List/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDo List
+//
+//  Created by AZM on 2020/11/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
