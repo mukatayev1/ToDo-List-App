c39aeb9e6dae19d9e8d849396c71ff2e70488ab7
Aidos Mukatayev <mukatayev19@gmail.com>
2020-11-05 19:29:01 +0900

    Initial Commit

--- ToDo List/ViewController.swift
+++ ToDo List/ViewController.swift
@@ -8,12 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //MARK: - Properties
+    
+    //MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
+    
+    //MARK: - Helpers
 
-
+    
 }
 
